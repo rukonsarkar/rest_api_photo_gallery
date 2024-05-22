5d21a6fc5da98bfe1fa40511f48dcf26118ecc4a
Rukon <developerrukon@gmail.com>
2024-05-22 22:42:20 +0600

    Committed

--- lib/photo_details.dart
+++ lib/photo_details.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class PhotoDetailsScreen extends StatefulWidget {
-
   final String image;
   final String title;
   final int id;
 
-  const PhotoDetailsScreen({super.key, required this.image, required this.title, required this.id});
-
+  const PhotoDetailsScreen(
+      {super.key, required this.image, required this.title, required this.id});
 
   @override
   State<PhotoDetailsScreen> createState() => _PhotoDetailsScreenState();
@@ -17,56 +17,103 @@ class PhotoDetailsScreen extends StatefulWidget {
 class _PhotoDetailsScreenState extends State<PhotoDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Photo Details',
-          style: TextStyle(
-              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+    if(MediaQuery.of(context).orientation == Orientation.portrait){
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Photo Details',
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.green,
+          iconTheme: const IconThemeData(color: Colors.white),
         ),
-        backgroundColor: Colors.green,
-        iconTheme: const IconThemeData(
-          color: Colors.white
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-       //   crossAxisAlignment: CrossAxisAlignment.center,
-       //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
-              child: Container(
-                height: 400,
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          widget.image),
-                      fit: BoxFit.fill),
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(10),
+        body: SingleChildScrollView(
+          child: Column(
+            //   crossAxisAlignment: CrossAxisAlignment.center,
+            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 50, 20, 20),
+                child: Container(
+                  height: 400,
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(widget.image), fit: BoxFit.fill),
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: Expanded(
-                child: Text(
-                  widget.title,
-                  style: TextStyle(fontSize: 18, color: Colors.black),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Expanded(
+                  child: Text(
+                    widget.title,
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
                 ),
               ),
-            ),
-
-            const SizedBox(height: 30),
-
-            Text('ID: ${widget.id}', style: const TextStyle(fontSize: 18, color: Colors.black)),
-          ],
+              const SizedBox(height: 30),
+              Text('ID: ${widget.id}',
+                  style: const TextStyle(fontSize: 18, color: Colors.black)),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Photo Details',
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.green,
+          iconTheme: const IconThemeData(color: Colors.white),
+        ),
+        body: SingleChildScrollView(
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  height: 250,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(widget.image), fit: BoxFit.fill),
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Expanded(
+                      child: Text(
+                        widget.title,
+                        style: const TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Text('ID: ${widget.id}',
+                      style: const TextStyle(fontSize: 18, color: Colors.black)),
+                ],
+                ),
+              )
+
+            ],
+          ),
+        ),
+      );
+    }
+
   }
 }
